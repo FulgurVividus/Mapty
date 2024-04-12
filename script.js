2da0d0c1e49f57ac7686984d0facaf912e67a95d
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-04-13 00:36:06 +0500

    feat: added geo-location API

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+//# Implementing geo-location API
+// accepts 2 parameters: success and error functions
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (parameter) {
+      // success
+      const { latitude } = parameter.coords;
+      const { longitude } = parameter.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},15.5z?entry=ttu`
+      );
+    },
+    function () {
+      // error
+      alert(`Could not get the current location 🗺️`);
+    }
+  );
+}
